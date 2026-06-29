fa5e7980da946b29d448a5682338d84a5409e67c
petrkrock <petrkoltsov33@gmail.com>
2026-06-29 21:06:01 +0000

    Add missing src folder with main.js and App.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="min-h-screen bg-brand-gray">
+    <template v-if="!auth.loading">
+      <template v-if="auth.user">
+        <AppHeader />
+        <main class="pb-20 md:pb-6">
+          <router-view />
+        </main>
+        <MobileNav />
+      </template>
+      <template v-else>
+        <router-view />
+      </template>
+    </template>
+    <template v-else>
+      <div class="min-h-screen flex items-center justify-center">
+        <div class="flex flex-col items-center gap-4">
+          <BrandLogo class="h-10" />
+          <div class="w-8 h-8 border-2 border-brand-red border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import AppHeader from '@/components/AppHeader.vue'
+import MobileNav from '@/components/MobileNav.vue'
+import BrandLogo from '@/components/BrandLogo.vue'
+
+const auth = useAuthStore()
+</script>
